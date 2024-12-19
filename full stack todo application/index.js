8108442b3f2813761d3fc8bd9790cc54ce70df68
hussain shamim <hj422748@gmail.com>
2024-12-19 13:28:30 +0500

    Full stack todo app

---
+++ full stack todo application/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 7000;
+
+app.get('/',(req,res)=>{
+    res.send("I Am Full Stack todo App")
+})
+
+app.listen(PORT,()=>{
+    console.log(`port is working at ${PORT}`)
+})
