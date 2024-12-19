3df421b552d1a50a72db0e8945aeaeff1de435cd
hussain shamim <hj422748@gmail.com>
2024-12-19 18:31:30 +0500

    sup folders

--- full stack todo application/Backend/index.js
+++ full stack todo application/Backend/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 7000;
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("I Am Full Stack todo App")
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`port is working at ${PORT}`)
 })
